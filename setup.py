2f4bcd2f00b44760b5fbebf1f8f9dee553b9d3db
luiscaballeroramos <63744250+luiscaballeroramos@users.noreply.github.com>
2025-06-17 00:27:38 +0200

    Comment out setup configuration in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-setup(
-    name="portfolio",
-    version="0.1.0",
-    packages=find_packages(include=["portfolio", "portfolio*"]),
-)
+# setup(
+#     name="portfolio",
+#     version="0.1.0",
+#     packages=find_packages(include=["portfolio", "portfolio*"]),
+# )
