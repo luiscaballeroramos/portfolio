df2073f6b138d5ac62932fa2a2940e83d39abfac
luiscaballeroramosDOSA <lcaballeror@dragadosoffshore.es>
2025-06-16 12:28:28 +0200

    Update package inclusion in setup.py to explicitly include the 'portfolio' package

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="portfolio",
     version="0.1.0",
-    packages=find_packages(include=["portfolio*"]),
+    packages=find_packages(include=["portfolio", "portfolio*"]),
 )
