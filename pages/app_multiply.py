ed82caf7f70da8b5b949c8d7f7fb80df83c431fe
luiscaballeroramosDOSA <lcaballeror@dragadosoffshore.es>
2025-06-16 13:19:19 +0200

    Refactor import and function call to use sqrt2 from lcr_code2.operaciones2

--- pages/app_multiply.py
+++ pages/app_multiply.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from lcr_code.operaciones import sqrt
+from lcr_code2.operaciones2 import sqrt2
 
 # Slider de 0 a 100
 valor = st.slider("Selecciona un valor", min_value=0, max_value=100, value=64)
 
 # Llamar a la función del otro archivo
-resultado = sqrt(valor)
+resultado = sqrt2(valor)
 
 # Mostrar el resultado en un cuadro de texto
 st.text_input("Squared root of value", value=str(resultado), disabled=True)
