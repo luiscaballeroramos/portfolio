084544824e803fbc233eab2757cb1fd082393d62
luiscaballeroramosDOSA <lcaballeror@dragadosoffshore.es>
2025-06-16 12:12:43 +0200

    Add initial setup and structure for the portfolio package

--- main.py
+++ main.py
@@ -2,7 +2,32 @@ import install_private_packages
 
 import streamlit as st
 
+# Configuración inicial
 st.set_page_config(page_title="main", layout="centered")
+
+# Estilo global para botones (afecta también a st.page_link)
+st.markdown(
+    """
+    <style>
+    /* Afecta botones en general, incluyendo los de st.page_link */
+    button[kind="link"] {
+        border: 2px solid #777777 !important;  /* Gris medio */
+        border-radius: 8px;
+        padding: 0.5em 1em;
+        margin: 0.3em 0;
+        color: #fafafa;
+        background-color: #0e1117;
+    }
+
+    button[kind="link"]:hover {
+        border-color: #aaaaaa;
+        background-color: #222222;
+    }
+    </style>
+""",
+    unsafe_allow_html=True,
+)
+
 st.title("Portfolio Luis Caballero Ramos\n🚧 Portfolio Under Construction")
 st.write("Select an app to run")
 col1, col2 = st.columns(2)
